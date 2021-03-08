f124299a1d03c0867bb98ac4d6fcf731e6510724
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-03-08 12:13:36 +0000

    Fixed route controller naming!

--- src/main/java/com/qa/choonz/rest/controller/RouteController.java
+++ src/main/java/com/qa/choonz/rest/controller/RouteController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class RouteContoller {
+public class RouteController {
     @GetMapping(value = "/")
     public String index() {
         return "index.html";

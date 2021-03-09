52780406332a297f1341e9752754b5123186042e
kidd <ekidd@qa.com>
2021-03-09 15:14:45 +0000

    CWP-62 fixed inability to create genres due to lack of setters/getters

--- src/main/java/com/qa/choonz/persistence/domain/Genre.java
+++ src/main/java/com/qa/choonz/persistence/domain/Genre.java
@@ -47,7 +47,39 @@ public class Genre {
         this.tracks = tracks;
     }
 
-    @Override
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public List<Track> getTracks() {
+		return tracks;
+	}
+
+	public void setTracks(List<Track> tracks) {
+		this.tracks = tracks;
+	}
+
+	@Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Genre [id=").append(id).append(", name=").append(name).append(", description=")

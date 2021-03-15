c1d7fc1c84fda9f4220c9219ed2802d079a57f62
Pjqa <>
2021-03-15 14:10:15 +0000

    CWP-92_Album_Domain_Test started testing album domain

--- src/test/java/com/qa/choonz_choonz/domain/AlbumTest.java
+++ src/test/java/com/qa/choonz_choonz/domain/AlbumTest.java
@@ -1,5 +1,38 @@
 package com.qa.choonz_choonz.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import com.qa.choonz.persistence.domain.Album;
+import com.qa.choonz.persistence.domain.Artist;
+import com.qa.choonz.persistence.domain.Genre;
+
 public class AlbumTest {
+
+	@Test
+	public void testConstructor1() {
+		Artist artist = new Artist(1L, "Ed Sheeran");
+		Genre genre = new Genre(1L, "Pop", "Pop music is the genre of popular music that produces the most hit");
+		Album album = new Album(1L, "+", artist, genre, "Cover");
+		assertEquals(Long.valueOf("1"), album.getId());
+		assertEquals("+", album.getName());
+		assertEquals(artist, album.getArtist());
+		assertEquals(genre, album.getGenre());
+		assertEquals("Cover", album.getCoverImg());
+	}
+
+
+	@Test
+	public void testHashCode() {
+		Album album = new Album(1L, "+", null, null, "Cover");
+		assertEquals(album.hashCode(), album.hashCode());
+	}
+	
+
+	@Test
+	public void testToString() {
+		Album album = new Album(1L, "+", null, null, "Cover");
+		String expected = "Album(id=1, name=+, tracks=null, artist=null, genre=null, coverImg=Cover)";
+		assertEquals(expected, album.toString());
+	}
 
 }
